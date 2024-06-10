210104dc61d942541361902623a14169f4570b0f
MOHAMED SESAY <162488876+deeeye2@users.noreply.github.com>
2024-06-10 19:48:07 +0200

    Create HomePage.js

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <h1>Welcome to DevOps-bot</h1>
+      <p>Find solutions to your DevOps problems or submit new issues.</p>
+    </div>
+  );
+};
+
+export default HomePage;
