4f9ff92fef895d9e3869097c74593ee30855043c
MOHAMED SESAY <162488876+deeeye2@users.noreply.github.com>
2024-06-10 19:47:02 +0200

    Create Footer.js

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="footer-content">
+                <p>&copy; 2024 DevOps Bot. All rights reserved.</p>
+                <p><a href="#">Privacy Policy</a> | <a href="#">Terms of Service</a></p>
+                <p className="scrolling-text">The purpose of this page is to help DevOps engineers solve DevOps tools problems, like installing, networking, and more. Built by Mohamed Sesay, a DevOps Engineer.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
